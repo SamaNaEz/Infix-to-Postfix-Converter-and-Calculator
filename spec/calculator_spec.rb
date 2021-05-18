fd28cf414ffceea641d53294402182ee6ec7c1ce
Carlos Soria <csoria@cultome.io>
2021-05-18 14:01:57 -0500

    Observations to code

--- spec/calculator_spec.rb
+++ spec/calculator_spec.rb
@@ -6,36 +6,26 @@ RSpec.describe Calculator_v2 do
   it 'Infix to postfix converter' do
     str = '( 6 + 2 ) * 5 - 8 / 4'
 
-    expect(cls.converter(str)).to eq '6 2 + 5 * 8 4 / -'
+    expect(cls.infix_to_postfix(str)).to eq '6 2 + 5 * 8 4 / -'
   end
 
   it 'Integer postfix evaluation' do
-    cls.evaluator('5 2 + 5 * 8 4 / -')
-
-    expect(cls.result).to eq 33
+    expect(cls.eval_postfix('5 2 + 5 * 8 4 / -')).to eq 33
   end
-  
-  it 'Decimal postfix evaluation' do
-    cls.evaluator('10.1 2 + 5 * 8 4 / -')
 
-    expect(cls.result).to eq 58.5
+  it 'Decimal postfix evaluation' do
+    expect(cls.eval_postfix('10.1 2 + 5 * 8 4 / -')).to eq 58.5
   end
 
   it 'Exponent evaluation' do
-    cls.global('5 ** 2')
-
-    expect(cls.result).to eq 25
+    expect(cls.calculate('5 ** 2')).to eq 25
   end
 
   it 'Remainder evaluation ' do
-    cls.global('5 % 3')
-
-    expect(cls.result).to eq 2
+    expect(cls.calculate('5 % 3')).to eq 2
   end
-  
-  it 'Global calculator evaluation' do
-    cls.global('( ( 10 - 8 ) + 20 - ( 3.5 * 2 ) / 4 ** 2 ) % 4')
 
-    expect(cls.result).to eq 1.5625
+  it 'Global calculator evaluation' do
+    expect(cls.calculate('( ( 10 - 8 ) + 20 - ( 3.5 * 2 ) / 4 ** 2 ) % 4')).to eq 1.5625
   end
 end
